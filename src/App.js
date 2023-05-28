7505a43c1bcdd17612a264bee0cee57dc6bb8dce
Chethan km <121347478+Chethankm4244@users.noreply.github.com>
2023-05-28 20:43:04 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import Header from "./components/Header";
 import MainPage from "./components/MainPage";
 import Rightnav from "./components/Rightnav";
 import Sidenav from "./components/Sidenav";
-
+import './components/App.css'
 
 function App() {
   return (
