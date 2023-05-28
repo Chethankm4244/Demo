b019383f8cf49f6b9ba5ba340303d9423016058c
Chethan km <121347478+Chethankm4244@users.noreply.github.com>
2023-05-28 20:39:54 +0530

    Update Rightnav.js

--- src/components/Rightnav.js
+++ src/components/Rightnav.js
@@ -1,18 +1,20 @@
-import React from 'react'
-import './Rightnav.css'
-import {BsArrowLeft} from 'react-icons/bs'
+import React from "react";
+import "./Rightnav.css";
+import { BsArrowLeft } from "react-icons/bs";
 
 const Rightnav = () => {
   return (
-    <div className='rightnav'>
-     <div className="header2">
-      <span className='name1'>0.2 $XYZ</span>
-      <span className='name2'>Tier1</span>
-     </div>
+    <div className="rightnav">
+      <div className="header2">
+        <span className="name1">0.2 $XYZ</span>
+        <span className="name2">Tier1</span>
+      </div>
       <div className="horizontalline2"> </div>
       <div className="content1">
-        
-        <span className='edit'>Edit Todo</span>
+        <div className="icon8">
+          <BsArrowLeft />
+        </div>
+        <span className="edit">Edit Todo</span>
       </div>
       <div className="content2">
         <span>Carrot</span>
@@ -21,13 +23,10 @@ const Rightnav = () => {
         <span>Carrot improves eyesight.</span>
       </div>
 
-      <button className='btn'>Save</button>
-      <div className="icon8"><BsArrowLeft/></div>
-
-
-
+      <button className="btn">Save</button>
     </div>
-  )
-}
+  );
+};
 
-export default Rightnav
+export default Rightnav;
+
